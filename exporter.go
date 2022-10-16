c17b79ac314b8ce478626f44e225b8dc0d304544
Luiz Motta <luiz.motta@itschmitz.com>
2022-10-16 20:40:00 -0300

    refactor: call exporter builders

--- exporter.go
+++ exporter.go
@@ -4,44 +4,14 @@ import (
 	"context"
 
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
-func createExporter(ctx context.Context, config *OpenTelemetryConfiguration) *otlptrace.Exporter {
+func buildTraceExporter(ctx context.Context, config *OpenTelemetryConfiguration) *otlptrace.Exporter {
 	if config.exporterProtocol == HTTP {
-		url := getHTTPexporterURL(config)
-		client := otlptracehttp.NewClient(otlptracehttp.WithEndpoint(url))
-		exporter, err := otlptrace.New(ctx, client)
-		if err != nil {
-			panic(err)
-		}
+		exporter := buildHTTPTraceExporter(ctx, config)
 		return exporter
 	}
-	url := getGRPCexporterURL(config)
-	connection, err := grpc.DialContext(ctx, url, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
-	if err != nil {
-		panic(err)
-	}
-	exporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(connection))
-	if err != nil {
-		panic(err)
-	}
+
+	exporter := buildGRPCTraceExporter(ctx, config)
 	return exporter
-}
-
-func getHTTPexporterURL(config *OpenTelemetryConfiguration) string {
-	if config.collectorURL == "" {
-		return default_HTTP_URL()
-	}
-	return config.collectorURL
-}
-
-func getGRPCexporterURL(config *OpenTelemetryConfiguration) string {
-	if config.collectorURL == "" {
-		return default_gRPC_URL()
-	}
-	return config.collectorURL
 }
