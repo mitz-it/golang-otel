9679539fde032d6ef54e39cc4fec0a13017715c7
Luiz Motta <luiz.motta@itschmitz.com>
2022-10-16 20:39:08 -0300

    chore: move default url builders

--- defaults.go
+++ defaults.go
@@ -1,7 +1,5 @@
 package golang_otel
 
-import "fmt"
-
 const default_version = "1.0.0"
 
 const localhost = "127.0.0.1"
@@ -9,11 +7,3 @@ const localhost = "127.0.0.1"
 const gRPC_port = 4317
 
 const http_port = 4318
-
-func default_gRPC_URL() string {
-	return fmt.Sprintf("%s:%d", localhost, gRPC_port)
-}
-
-func default_HTTP_URL() string {
-	return fmt.Sprintf("%s:%d", localhost, http_port)
-}
