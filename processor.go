2b74e7c7651757e9fa4b042873501b86c350e10e
lgcmotta <luiz.motta@itschmitz.com>
2022-12-12 15:00:06 -0300

    feat: build processor based on processor type and exporter

--- processor.go
+++ processor.go
@@ -2,25 +2,47 @@ package otel
 
 import (
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
-func buildSpanProcessor(config *OpenTelemetryConfiguration, exporter *otlptrace.Exporter) sdktrace.SpanProcessor {
-	if config.spanProcessorType == BATCH {
-		spanProcessor := buildBatchSpanProcessor(exporter)
+func buildSpanProcessor(config *OpenTelemetryConfiguration, exporters *exporters) sdktrace.SpanProcessor {
+	if exporter := exporters.otlp; config.exporterProtocol == GRPC || config.exporterProtocol == HTTP {
+		if config.spanProcessorType == BATCH {
+			spanProcessor := buildBatchSpanProcessorForOTLP(exporter)
+			return spanProcessor
+		}
+		spanProcessor := buildSimpleSpanProcessorForOTLP(exporter)
 		return spanProcessor
 	}
 
-	spanProcessor := buildSimpleSpanProcessor(exporter)
+	exporter := exporters.stdout
+
+	if config.spanProcessorType == BATCH {
+		spanProcessor := buildBatchSpanProcessorForSTDOUT(exporter)
+		return spanProcessor
+	}
+	spanProcessor := buildSimpleSpanProcessorForSTDOUT(exporter)
 	return spanProcessor
+
 }
 
-func buildBatchSpanProcessor(exporter *otlptrace.Exporter) sdktrace.SpanProcessor {
+func buildBatchSpanProcessorForOTLP(exporter *otlptrace.Exporter) sdktrace.SpanProcessor {
 	batchSpanProcessor := sdktrace.NewBatchSpanProcessor(exporter)
 	return batchSpanProcessor
 }
 
-func buildSimpleSpanProcessor(exporter *otlptrace.Exporter) sdktrace.SpanProcessor {
+func buildBatchSpanProcessorForSTDOUT(exporter *stdouttrace.Exporter) sdktrace.SpanProcessor {
+	batchSpanProcessor := sdktrace.NewBatchSpanProcessor(exporter)
+	return batchSpanProcessor
+}
+
+func buildSimpleSpanProcessorForOTLP(exporter *otlptrace.Exporter) sdktrace.SpanProcessor {
+	simpleSpanProcessor := sdktrace.NewSimpleSpanProcessor(exporter)
+	return simpleSpanProcessor
+}
+
+func buildSimpleSpanProcessorForSTDOUT(exporter *stdouttrace.Exporter) sdktrace.SpanProcessor {
 	simpleSpanProcessor := sdktrace.NewSimpleSpanProcessor(exporter)
 	return simpleSpanProcessor
 }
