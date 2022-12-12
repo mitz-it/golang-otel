3d78bbf6ac485134137af73bf242c2cfe567f017
lgcmotta <luiz.motta@itschmitz.com>
2022-12-12 14:59:46 -0300

    feat: support otlp and stdout exporters

--- exporter.go
+++ exporter.go
@@ -4,14 +4,32 @@ import (
 	"context"
 
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 )
 
-func buildTraceExporter(ctx context.Context, config *OpenTelemetryConfiguration) *otlptrace.Exporter {
+type exporters struct {
+	otlp   *otlptrace.Exporter
+	stdout *stdouttrace.Exporter
+}
+
+func newExporters(otlp *otlptrace.Exporter, stdout *stdouttrace.Exporter) *exporters {
+	return &exporters{
+		otlp:   otlp,
+		stdout: stdout,
+	}
+}
+
+func buildTraceExporter(ctx context.Context, config *OpenTelemetryConfiguration) *exporters {
+	if config.exporterProtocol == STDOUT {
+		exporter := buildStdoutTraceExporter()
+		return newExporters(nil, exporter)
+	}
+
 	if config.exporterProtocol == HTTP {
 		exporter := buildHTTPTraceExporter(ctx, config)
-		return exporter
+		return newExporters(exporter, nil)
 	}
 
 	exporter := buildGRPCTraceExporter(ctx, config)
-	return exporter
+	return newExporters(exporter, nil)
 }
