51978b365f413d8ab388afe1ccb3f8dd8078a558
lgcmotta <luiz.motta@itschmitz.com>
2022-12-12 15:14:22 -0300

    refactor: create span processor with switch exporter type

--- processor.go
+++ processor.go
@@ -7,24 +7,38 @@ import (
 )
 
 func buildSpanProcessor(config *OpenTelemetryConfiguration, exporters *exporters) sdktrace.SpanProcessor {
-	if exporter := exporters.otlp; config.exporterProtocol == GRPC || config.exporterProtocol == HTTP {
-		if config.spanProcessorType == BATCH {
-			spanProcessor := buildBatchSpanProcessorForOTLP(exporter)
-			return spanProcessor
-		}
-		spanProcessor := buildSimpleSpanProcessorForOTLP(exporter)
-		return spanProcessor
+	switch config.exporterProtocol {
+	case GRPC:
+		exporter := exporters.grpc
+		return buildSpanProcessorForOTLP(config, exporter)
+	case HTTP:
+		exporter := exporters.http
+		return buildSpanProcessorForOTLP(config, exporter)
+	case STDOUT:
+		exporter := exporters.stdout
+		return buildSpanProcessorForSTDOUT(config, exporter)
+	default:
+		exporter := exporters.grpc
+		return buildSpanProcessorForOTLP(config, exporter)
 	}
+}
 
-	exporter := exporters.stdout
-
+func buildSpanProcessorForSTDOUT(config *OpenTelemetryConfiguration, exporter *stdouttrace.Exporter) sdktrace.SpanProcessor {
 	if config.spanProcessorType == BATCH {
 		spanProcessor := buildBatchSpanProcessorForSTDOUT(exporter)
 		return spanProcessor
 	}
 	spanProcessor := buildSimpleSpanProcessorForSTDOUT(exporter)
 	return spanProcessor
+}
 
+func buildSpanProcessorForOTLP(config *OpenTelemetryConfiguration, exporter *otlptrace.Exporter) sdktrace.SpanProcessor {
+	if config.spanProcessorType == BATCH {
+		spanProcessor := buildBatchSpanProcessorForOTLP(exporter)
+		return spanProcessor
+	}
+	spanProcessor := buildSimpleSpanProcessorForOTLP(exporter)
+	return spanProcessor
 }
 
 func buildBatchSpanProcessorForOTLP(exporter *otlptrace.Exporter) sdktrace.SpanProcessor {
