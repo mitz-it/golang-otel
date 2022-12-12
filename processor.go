0cfa6941ced978a82f8ec66f6a0aac980f6fe50b
lgcmotta <luiz.motta@itschmitz.com>
2022-12-12 18:06:33 -0300

    refactor: remove redundant code

--- processor.go
+++ processor.go
@@ -25,38 +25,18 @@ func buildSpanProcessor(config *OpenTelemetryConfiguration, exporters *exporters
 
 func buildSpanProcessorForSTDOUT(config *OpenTelemetryConfiguration, exporter *stdouttrace.Exporter) sdktrace.SpanProcessor {
 	if config.spanProcessorType == BATCH {
-		spanProcessor := buildBatchSpanProcessorForSTDOUT(exporter)
+		spanProcessor := sdktrace.NewBatchSpanProcessor(exporter)
 		return spanProcessor
 	}
-	spanProcessor := buildSimpleSpanProcessorForSTDOUT(exporter)
+	spanProcessor := sdktrace.NewSimpleSpanProcessor(exporter)
 	return spanProcessor
 }
 
 func buildSpanProcessorForOTLP(config *OpenTelemetryConfiguration, exporter *otlptrace.Exporter) sdktrace.SpanProcessor {
 	if config.spanProcessorType == BATCH {
-		spanProcessor := buildBatchSpanProcessorForOTLP(exporter)
+		spanProcessor := sdktrace.NewBatchSpanProcessor(exporter)
 		return spanProcessor
 	}
-	spanProcessor := buildSimpleSpanProcessorForOTLP(exporter)
+	spanProcessor := sdktrace.NewSimpleSpanProcessor(exporter)
 	return spanProcessor
-}
-
-func buildBatchSpanProcessorForOTLP(exporter *otlptrace.Exporter) sdktrace.SpanProcessor {
-	batchSpanProcessor := sdktrace.NewBatchSpanProcessor(exporter)
-	return batchSpanProcessor
-}
-
-func buildBatchSpanProcessorForSTDOUT(exporter *stdouttrace.Exporter) sdktrace.SpanProcessor {
-	batchSpanProcessor := sdktrace.NewBatchSpanProcessor(exporter)
-	return batchSpanProcessor
-}
-
-func buildSimpleSpanProcessorForOTLP(exporter *otlptrace.Exporter) sdktrace.SpanProcessor {
-	simpleSpanProcessor := sdktrace.NewSimpleSpanProcessor(exporter)
-	return simpleSpanProcessor
-}
-
-func buildSimpleSpanProcessorForSTDOUT(exporter *stdouttrace.Exporter) sdktrace.SpanProcessor {
-	simpleSpanProcessor := sdktrace.NewSimpleSpanProcessor(exporter)
-	return simpleSpanProcessor
 }
